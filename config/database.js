6598768bc9f2c6837b421b5ef0b2a69e4b3743e5
Eric Johnson <erjohns3@illinois.edu>
2018-04-07 15:59:37 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://localhost:27017/meanapp1',     // local database --might want to change to 'mongodb://localhost:27017/messagingapp' for confusion
-    database: 'mongodb://eric:Fd@tst1109@ds239009.mlab.com:39009/messagingapp',
+    database: 'mongodb://eric:Dinoborf321@ds239009.mlab.com:39009/messagingapp',
     secret: 'secret'
 };
